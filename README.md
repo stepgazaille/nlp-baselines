4f2de7149de98a86eea1360224138c41dcf4e039
stepgazaille <stepgazaille@gmail.com>
2022-04-13 12:39:59 -0400

    bug fixes for batch_size > 1 and results logging

--- README.md
+++ README.md
@@ -12,11 +12,12 @@ conda env create
 ## Usage
 ```
 $ python sequence_classifier.py -h
-usage: sequence_classifier.py [-h] [-d DATASET] [-m MODEL_NAME_OR_PATH] [-g GPUS] [-l LOG_DIR] [-b BATCH_SIZE] [-e MAX_EPOCHS] [-w NUM_WORKERS] [-s SEED] [-p {64,32,16}]
+usage: sequence_classifier.py [-h] [-d {ag_news}] [-m MODEL_NAME_OR_PATH] [-g GPUS] [-l LOG_DIR] [-b BATCH_SIZE] [-e MAX_EPOCHS] [-r LEARNING_RATE] [-w NUM_WORKERS]
+                              [-s SEED] [-p {64,32,16}]
 
 optional arguments:
   -h, --help            show this help message and exit
-  -d DATASET, --dataset DATASET
+  -d {ag_news}, --dataset {ag_news}
                         The dataset ID of a sequence classification dataset hosted inside a dataset repo on huggingface.co (default: ag_news)
   -m MODEL_NAME_OR_PATH, --model-name-or-path MODEL_NAME_OR_PATH
                         The model ID of a pretrained model hosted inside a model repo on huggingface.co (default: distilbert-base-uncased)
@@ -24,13 +25,18 @@ optional arguments:
   -l LOG_DIR, --log-dir LOG_DIR
                         Default path for logs and weights (default: ./)
   -b BATCH_SIZE, --batch-size BATCH_SIZE
-                        How many samples per batch to load. (default: 4)
+                        How many samples per batch to load. (default: 8)
   -e MAX_EPOCHS, --max-epochs MAX_EPOCHS
                         Stop training once this number of epochs is reached. (default: 100)
+  -r LEARNING_RATE, --learning-rate LEARNING_RATE
+                        Learning rate. (default: 2e-05)
   -w NUM_WORKERS, --num-workers NUM_WORKERS
                         Number of subprocesses to use for data loading. (default: 4)
   -s SEED, --seed SEED  The integer value seed for global random state. (default: 42)
   -p {64,32,16}, --precision {64,32,16}
                         Use double precision (64), full precision (32) or half precision (16). (default: 16)
 ```
-By defTensorboard logs
+To visualize Tensorboard logs:
+```
+tensorboard --logdir lightning_logs
+```
--- sequence_classifier.py
+++ sequence_classifier.py
@@ -5,12 +5,12 @@ from pathlib import Path
 import json
 import torch
 from torch import Tensor, argmax
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 from torch.nn import CrossEntropyLoss
 from torch.optim import Optimizer
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer, seed_everything
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from datasets import load_dataset, load_metric
+from datasets import load_dataset, load_metric, DatasetDict
 from datasets.arrow_dataset import Batch
 from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification
 from transformers.modeling_outputs import SequenceClassifierOutput
@@ -24,7 +24,6 @@ class Dataset(LightningDataModule):
 					   model_name_or_path: str,
 					   batch_size: int) -> None:
 		super().__init__()
-		self.save_hyperparameters()
 		self.dataset = load_dataset(dataset_name_or_path)		
 		self.label_names = self.dataset['train'].features['label'].names
 		self.batch_size = batch_size
@@ -33,14 +32,17 @@ class Dataset(LightningDataModule):
 
 	def setup(self, stage: str = None) -> None:		
 		self.label_binarizer.fit(range(len(self.label_names)))
+		val_split_size = len(self.dataset['test'])
+		new_train_split_size = len(self.dataset['train']) - val_split_size	
+		new_splits = self.dataset['train'].train_test_split(val_split_size, new_train_split_size)
+		self.dataset = DatasetDict({'train': new_splits['train'],
+									'val': new_splits['test'],
+									'test': self.dataset['test']})
 		self.dataset = self.dataset.map(self.preproc,
 										batched=True,
 										batch_size=self.batch_size,
 										remove_columns=['text', 'label'])
-		self.dataset.set_format(type='torch')
-		train_split_size = len(self.dataset['train']) - len(self.dataset['test'])
-		val_split_size = len(self.dataset['test'])
-		self.dataset['train'], self.dataset['val'] = random_split(self.dataset['train'], [train_split_size, val_split_size])		
+		self.dataset.set_format(type='torch')			
 
 	def preproc(self, batch: Batch) -> dict:
 		input_ids = self.tokenizer(batch['text'], padding=True)['input_ids']
@@ -48,7 +50,7 @@ class Dataset(LightningDataModule):
 		return {'input_ids': input_ids, 'labels': labels}
 	
 	def train_dataloader(self) -> DataLoader:
-		return DataLoader(self.dataset['train'], batch_size=self.batch_size, num_workers=args.num_workers, shuffle=True)
+		return DataLoader(self.dataset['train'], batch_size=self.batch_size, num_workers=args.num_workers)
 	
 	def val_dataloader(self) -> DataLoader:
 		return DataLoader(self.dataset['val'], batch_size=self.batch_size, num_workers=args.num_workers)
@@ -59,23 +61,21 @@ class Dataset(LightningDataModule):
 
 class SequenceClassifier(LightningModule):
 	
-	def __init__(self, model_name_or_path: str,
-					   label_names: list[str]) -> None:
+	def __init__(self, learning_rate: float, model_name_or_path: str, label_names: list[str]) -> None:
 		super().__init__()
-		self.save_hyperparameters()
+		self.learning_rate = learning_rate
 		self.label_names = label_names
 		self.config = AutoConfig.from_pretrained(model_name_or_path, num_labels=len(self.label_names))
 		self.model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path, config=self.config)
 		self.loss_function = CrossEntropyLoss()
 		experiment_id = datetime.now()
 		self.f1 = load_metric('f1', experiment_id=experiment_id)
-		self.accuracy = load_metric('accuracy', experiment_id=experiment_id)
 							
 	def forward(self, input_ids: Tensor) -> SequenceClassifierOutput:
 		return self.model(input_ids)
 
 	def configure_optimizers(self) -> Optimizer:
-		return  torch.optim.Adam(self.parameters(), lr=.0002)
+		return  torch.optim.Adam(self.parameters(), lr=self.learning_rate)
 
 	def training_step(self, batch: dict, batch_idx: int) -> Tensor:
 		logits  = self(batch['input_ids'])['logits']
@@ -94,14 +94,14 @@ class SequenceClassifier(LightningModule):
 		self.test_labels = []
 
 	def test_step(self, batch: dict, batch_idx: int) -> None:
-		logits  = self(batch['input_ids'])['logits']
+		logits = self(batch['input_ids'])['logits']
 		self.test_preds += argmax(logits, axis=1).tolist()
 		self.test_labels += argmax(batch['labels'], axis=1).tolist()
 	
 	def on_test_epoch_end(self) -> None:
 		self.log_dict(self.f1.compute(predictions=self.test_preds, references=self.test_labels, average='micro'))
-		self.log_dict(self.f1.compute(predictions=self.test_preds, references=self.test_labels, average=None, labels=self.label_names))
-		self.log_dict(self.accuracy.compute(predictions=self.test_preds, references=self.test_labels))
+		classwise_f1_scores = self.f1.compute(predictions=self.test_preds, references=self.test_labels, average=None)['f1']		
+		self.log_dict(dict(zip(self.label_names, classwise_f1_scores)))
 
 
 if __name__ == '__main__':
@@ -111,8 +111,9 @@ if __name__ == '__main__':
 	arg_parser.add_argument('-m', '--model-name-or-path', default='distilbert-base-uncased', type=str, help="The model ID of a pretrained model hosted inside a model repo on huggingface.co")
 	arg_parser.add_argument('-g', '--gpus', default='[0]', type=str, help="Which GPUs to train on.")
 	arg_parser.add_argument('-l', '--log-dir', default='./', type=str, help="Default path for logs and weights ")
-	arg_parser.add_argument('-b', '--batch-size', default=2, type=int, help="How many samples per batch to load.")
-	arg_parser.add_argument('-e', '--max-epochs', default=5, type=int, help="Stop training once this number of epochs is reached.")
+	arg_parser.add_argument('-b', '--batch-size', default=8, type=int, help="How many samples per batch to load.")
+	arg_parser.add_argument('-e', '--max-epochs', default=100, type=int, help="Stop training once this number of epochs is reached.")
+	arg_parser.add_argument('-r', '--learning-rate', default=2e-5, type=float, help="Learning rate.")
 	arg_parser.add_argument('-w', '--num-workers', default=cpu_count(), type=int, help="Number of subprocesses to use for data loading.")
 	arg_parser.add_argument('-s', '--seed', default=42, type=int, help="The integer value seed for global random state.")
 	arg_parser.add_argument('-p', '--precision', default=16, choices=[64, 32, 16], type=int, help="Use double precision (64), full precision (32) or half precision (16).")
@@ -126,7 +127,7 @@ if __name__ == '__main__':
 	dataset.setup()
 
 	print("Preparing the model...")
-	model = SequenceClassifier(args.model_name_or_path, dataset.label_names)
+	model = SequenceClassifier(args.learning_rate, args.model_name_or_path, dataset.label_names)
 	trainer = Trainer(accelerator='gpu',
 					  gpus=args.gpus,
 					  max_epochs=args.max_epochs,
