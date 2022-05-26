0f354cc48820bd36fa7392d3cf473c4bdc6a2daa
stepgazaille <stepgazaille@gmail.com>
2022-05-26 08:10:03 -0400

    dataset caching bugfix

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+from copy import copy
 from datasets import DatasetDict, load_dataset
 from datasets.arrow_dataset import Batch
 from pytorch_lightning import LightningDataModule
@@ -13,14 +14,15 @@ class Dataset(LightningDataModule):
 					   batch_size: int,
 					   num_workers: int) -> None:
 		super().__init__()
-		self.dataset = load_dataset(dataset_name_or_path)		
-		self.label_names = self.dataset['train'].features['label'].names
+		self.original_dataset = load_dataset(dataset_name_or_path)		
+		self.label_names = self.original_dataset['train'].features['label'].names
 		self.batch_size = batch_size
 		self.num_workers = num_workers
 		self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
 		self.label_binarizer = LabelBinarizer()
 
-	def setup(self, stage: str = None) -> None:		
+	def setup(self, stage: str = None) -> None:	
+		self.dataset = copy(self.original_dataset) # workaround to enable using cached datasets
 		self.label_binarizer.fit(range(len(self.label_names)))
 		val_split_size = len(self.dataset['test'])
 		new_train_split_size = len(self.dataset['train']) - val_split_size	
@@ -32,7 +34,7 @@ class Dataset(LightningDataModule):
 										batched=True,
 										batch_size=self.batch_size,
 										remove_columns=['text', 'label'])
-		self.dataset.set_format(type='torch')			
+		self.dataset.set_format(type='torch')
 
 	def preproc(self, batch: Batch) -> dict:
 		input_ids = self.tokenizer(batch['text'], padding=True)['input_ids']
--- train_sequence_classifier.py
+++ train_sequence_classifier.py
@@ -23,7 +23,8 @@ if __name__ == '__main__':
 	arg_parser.add_argument('-b', '--batch-size', default=8, type=int, help="How many samples per batch to load")
 	arg_parser.add_argument('-e', '--max-epochs', default=100, type=int, help="Stop training once this number of epochs is reached")
 	arg_parser.add_argument('-r', '--learning-rate', default=2.0e-5, type=float, help="Learning rate")
-	arg_parser.add_argument('--auto-lr-find', default=False, action='store_true', help="Try to optimize initial learning rate for faster convergence. The \"--learning-rate\" parameter is ignored when set to True")
+	arg_parser.add_argument('--auto-lr-find', default=False, action='store_true',
+							help="Try to optimize initial learning rate for faster convergence")
 	arg_parser.add_argument('-w', '--num-workers', default=cpu_count(), type=int, help="Number of subprocesses to use for data loading")
 	arg_parser.add_argument('-s', '--seed', default=42, type=int, help="The integer value seed for global random state")
 	arg_parser.add_argument('-p', '--precision', default=64, choices=[64, 32, 16], type=int,
@@ -34,11 +35,8 @@ if __name__ == '__main__':
 	logger = TensorBoardLogger(save_dir=args.log_dir, name=str(args.dataset))
 	seed_everything(args.seed)
 
-	print("Preprocessing the data...")
-	dataset = Dataset(args.dataset, args.model_name_or_path, args.batch_size, args.num_workers)
-	dataset.setup()
-
-	print("Preparing the model...")
+	# Preparing the model, data, and trainer:
+	dataset = Dataset(args.dataset, args.model_name_or_path, args.batch_size, args.num_workers)	
 	model = SequenceClassifier(args.learning_rate, args.model_name_or_path, dataset.label_names)
 	trainer = Trainer(accelerator='gpu',
 					  gpus=args.gpus,
@@ -50,18 +48,19 @@ if __name__ == '__main__':
 					  callbacks=[EarlyStopping(monitor='val_loss', mode='min')])
 	
 	if args.auto_lr_find:
-		trainer.tune(model,
-					 train_dataloaders=dataset.train_dataloader(),
-					 val_dataloaders=dataset.val_dataloader())
-		args.learning_rate = model.learning_rate
-
-
-	print("Start training the model...")
+		trainer.tune(model, datamodule=dataset)
+	else:
+		dataset.setup()
+	
+	# Document hyper-parameters:		
+	args.learning_rate = model.learning_rate
 	hparams = {k: vars(args)[k] for k in vars(args) if k != 'log_dir'}
 	logger.log_hyperparams(hparams)
+
+	# Start training the model:
 	training_start_time = datetime.now()
-	trainer.fit(model, dataset.train_dataloader(), dataset.val_dataloader())
+	trainer.fit(model, datamodule=dataset)
 	print(f"Training completed! Duration:{datetime.now() - training_start_time}")
 
 	print("Evaluating the best model...")
-	trainer.test(ckpt_path='best', dataloaders=dataset.test_dataloader())
+	trainer.test(ckpt_path='best', datamodule=dataset)
